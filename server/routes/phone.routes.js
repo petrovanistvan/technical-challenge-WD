f704321c6df7bd6940b6f384c6f0c0988c52c771
Istvan Petrovan <petrovanistvan@gmail.com>
2023-03-22 16:32:25 +0100

    first iteration done

--- server/routes/phone.routes.js
+++ server/routes/phone.routes.js
@@ -3,20 +3,17 @@ const phones = require('../data/phones.json');
 
 // get all phones
 router.get("/phones", (req, res, next) => {
-    res.json(JSON.stringify(phones));
-  });
+    res.json(phones)  
+});
 
 // get one phone by id
-router.get('/phones/:id', async (req, res, next) => {
-    try {
-        const phoneId = req.params.id
-        console.log(phoneId);
-        const selectedProduct = await phones.id
-        res.json(selectedProduct)
-    }
-    catch (error) {
-           console.log(error)
-    }
-})
+router.get('/phones/:id', function(req, res) {
+    let id = req.params.id;
+    for (let i = 0; i < phones.length; i++) {
+        if (phones[i].id == id) {
+            res.json(phones[i]);
+        } 
+    } 
+});
 
 module.exports = router;
