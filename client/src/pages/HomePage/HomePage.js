d66e5a257d479428968b697764c0715ba5cbc379
Istvan Petrovan <petrovanistvan@gmail.com>
2023-03-22 17:01:13 +0100

    displayed all phones and added basic styling

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,12 +1,40 @@
-import "./HomePage.css";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
+export default function HomePage() {
+  const [phones, setPhones] = useState([]);
+  const fetchPhones = async () => {
+    try {
+      const response = await fetch("http://localhost:5005/phones");
+      const parsed = await response.json();
+      setPhones(parsed);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-function HomePage() {
-  return (
-    <div>
-      <h1>Home page</h1>
-    </div>
-  );
+  useEffect(() => {
+    fetchPhones();
+  }, []);
+
+ return (
+  <>
+    <h1>Browse all of our phones:</h1>
+    <ul style={{listStyleType: "none", display: "flex", flexWrap: "wrap"}}>
+      {phones.map(phone => (
+        <li key={phone._id} style={{flex: "0 0 33.333333%"}}>
+          <div style={{width: "300px", height: "auto", backgroundColor: "white", padding: "10px", margin: "10px", borderRadius: "5%"}}>
+          <Link to={`/phones/${phone._id}`}><p style={{color: "black"}}>{phone.name}</p>
+          <img style={{width: "90%", height: "auto", color: "black"}} src={phone.imageFileName} alt="this is a pic"/>
+          <p style={{color: "black"}}>€{phone.price}</p>
+          </Link>
+          </div>
+        </li>
+      ))}
+    </ul>
+  </>
+ )
+
+  
+ 
 }
-
-export default HomePage;
